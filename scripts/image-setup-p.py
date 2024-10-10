0a34fddf88778a1088df745cdf55f8f18937058f
Duncan Jones <dj223@outlook.com>
2024-10-10 22:49:09 +0100

    Updated image-setup-p.py script to auto unmount, uncompress, and flash SD card

--- scripts/image-setup-p.py
+++ scripts/image-setup-p.py
@@ -2,6 +2,7 @@ import os
 import re
 import sys
 from pathlib import Path
+import time
 
 
 class Alt:
@@ -16,20 +17,25 @@ class Alt:
         return item == self.primary_o or item in self.alts_o
 
 
-class JuntPath(Path):
+class PathAdder(Path):
 
     def __add__(self, other):
         # Not sure internal variable.. this is easier
         if isinstance(other, str):
-            return JuntPath(os.path.join(str(self), other))
+            return PathAdder(os.path.join(str(self), other))
         elif isinstance(other, Path):
-            return JuntPath(os.path.join(str(self), str(other)))
+            return PathAdder(os.path.join(str(self), str(other)))
         else:
-            # Rust makes me think about other cases!
             raise ValueError(f"Unknown how to add to path type {type(other)}!")
 
 
-def find_most_recent_file_dir(path: JuntPath, regex=None):
+
+class FnWrap:
+    pass
+
+
+
+def find_most_recent_file_dir(path: PathAdder, regex=None):
     dir_contents = os.listdir(path)
 
     last_modified = path + dir_contents[0]
@@ -44,13 +50,13 @@ def find_most_recent_file_dir(path: JuntPath, regex=None):
                 last_modified_time = modified_time
                 last_modified = abs_name
 
-    return last_modified
+    return last_modified, last_modified_time
 
 
 def main(**kwargs):
-    sda_p = JuntPath("/dev/sda")
-    sda1_p = JuntPath("/dev/sda1")
-    sad2_p = JuntPath("/dev/sda2")
+    sda_p = PathAdder("/dev/sda")
+    sda1_p = PathAdder("/dev/sda1")
+    sad2_p = PathAdder("/dev/sda2")
 
     if not (sda_p.exists() and sda1_p.exists() and sad2_p.exists()):
         raise FileNotFoundError("Failed to parse drive..")
@@ -60,11 +66,38 @@ def main(**kwargs):
 
     print("Unmounted SDAs...")
 
-    images_p = JuntPath("build/tmp/deploy/images/")
+    images_p = PathAdder("tmp/deploy/images")
 
-    most_recent_dir = find_most_recent_file_dir(images_p)
+    most_recent_dir, _ = find_most_recent_file_dir(images_p)
 
-    find_most_recent_file_dir(most_recent_dir, re.compile(r".*\.wic\.bz2$"))
+    print(f"Most recent target system: {most_recent_dir.parts[-1]}")
+
+    for f in [most_recent_dir + "image.wic.bz2", most_recent_dir + "image.wic"]:
+        if f.exists():
+            os.remove(str(f))
+
+    most_recent_file, most_recent_time = find_most_recent_file_dir(most_recent_dir, re.compile(r".*\.wic\.bz2$"))
+
+    print(f"Found {most_recent_file.parts[-1]} created at {time.ctime(most_recent_time)}...")
+
+    print(f"Uncompressing {most_recent_file.parts[-1]}...")
+
+
+    new_file_path = most_recent_file.with_name(f'image.wic.bz2')
+
+    os.system(f"cp {most_recent_file} {new_file_path}")
+
+    os.system(f"bunzip2 {new_file_path}")
+
+    print("Flashing...")
+    os.system(f"sudo dd if={new_file_path.with_name('image.wic')} of=/dev/sda bs=4M status=progress conv=fsync")
+
+    os.system("sync")
+
+    print("Preparing to eject...")
+    os.system("sudo eject /dev/sda")
+
+    print("\nSD card is ready to eject...")
 
 
 def print_help():
@@ -92,6 +125,7 @@ def parse_arguements(args):
 
 
 if __name__ == '__main__':
-    args = sys.argv[1:]
-    kwargs = parse_arguements(args)
+    # args = sys.argv[1:]
+    # kwargs = parse_arguements(args)
+    kwargs = {}
     main(**kwargs)
